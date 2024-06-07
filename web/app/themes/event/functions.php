c3fd91f3bf6ac90455621b2015b718f8533704de
quentinNicolasTessier <quentin.nicolas.tessier@gmail.com>
2024-06-07 10:09:44 +0200

    clean function file

--- web/app/themes/event/functions.php
+++ web/app/themes/event/functions.php
@@ -1,43 +1,45 @@
 <?php
 
 // Ajouter la prise en charge des images mises en avant
-add_theme_support( 'post-thumbnails' );
+add_theme_support('post-thumbnails');
 
 // Ajouter automatiquement le titre du site dans l'en-tête du site
-add_theme_support( 'title-tag' );
+add_theme_support('title-tag');
 
-wp_enqueue_style( 'style', get_stylesheet_uri() );
+wp_enqueue_style('style', get_stylesheet_uri());
 
-register_nav_menus( array(
+register_nav_menus([
     'main' => 'Menu Principal',
     'footer' => 'Bas de page',
-) );
-add_action( 'init', function() {
-    register_extended_post_type( 'event' );
-} );
+]);
+add_action('init', function () {
+    register_extended_post_type('event');
+});
 
-add_action( 'init', function() {
-    register_extended_post_type( 'inscription event' );
-} );
+add_action('init', function () {
+    register_extended_post_type('inscription event');
+});
 
 // Compter les inscriptions pour un événement donné
-function compter_inscriptions($evenement_id) {
-    $args = array(
+function compter_inscriptions($evenement_id)
+{
+    $args = [
         'post_type' => 'inscription event',
-        'meta_query' => array(
-            array(
+        'meta_query' => [
+            [
                 'key' => 'evenement_id',
                 'value' => $evenement_id,
                 'compare' => '='
-            )
-        )
-    );
+            ]
+        ]
+    ];
 
     $inscriptions = new WP_Query($args);
     return $inscriptions->found_posts;
 }
 // Gérer la soumission du formulaire d'inscription à l'événement
-function gerer_inscription_evenement() {
+function gerer_inscription_evenement()
+{
     // Vérifier la présence et la validité des champs
     if (isset($_POST['nom'], $_POST['prenom'], $_POST['email'], $_POST['date_naissance'], $_POST['status'], $_POST['evenement_id']) && is_email($_POST['email'])) {
         $nom = sanitize_text_field($_POST['nom']);
@@ -54,29 +56,29 @@ function gerer_inscription_evenement() {
         $unlimited_place = get_field('unlimited_place', $evenement_id);
         if ($unlimited_place === true  || ($unlimited_place === false && $inscriptions_count < $nombre_places)) {
             // Enregistrer l'inscription dans la base de données
-            $inscription_id = wp_insert_post(array(
+            $inscription_id = wp_insert_post([
                 'post_title' => $nom,
                 'post_type' => 'inscription event',
                 'post_status' => 'publish',
-                'meta_input' => array(
-                    'nom'=> $nom,
-                    'prenom'=> $prenom,
-                    'date_naissance'=> $dateNaissance,
-                    'status'=> $status,
+                'meta_input' => [
+                    'nom' => $nom,
+                    'prenom' => $prenom,
+                    'date_naissance' => $dateNaissance,
+                    'status' => $status,
                     'email' => $email,
                     'evenement_id' => $evenement_id,
-                ),
-            ));
-            if($unlimited_place === false){
+                ],
+            ]);
+            if ($unlimited_place === false) {
                 update_field('number_available_place', $places_restantes - 1, $evenement_id);
             }
             $billet_entree = get_field('billet');
-            $url_billet=$billet_entree ? wp_get_attachment_url($billet_entree) : '';
-            if($billet_entree){
+            $url_billet = $billet_entree ? wp_get_attachment_url($billet_entree) : '';
+            if ($billet_entree) {
                 $to = $email;
                 $subject = 'Confirmation d\'inscription à l\'événement';
                 $body = 'Merci pour votre inscription à l\'événement. Vous pouvez télécharger votre billet d\'entrée en suivant ce lien : ' . esc_url($url_billet);
-                $headers = array('Content-Type: text/html; charset=UTF-8');
+                $headers = ['Content-Type: text/html; charset=UTF-8'];
                 wp_mail($to, $subject, $body, $headers);
             }
             // Rediriger vers une page de confirmation
